cd6afc2bdc6b2e4bb298a12cd4d67c699428e8e1
HEBCKUU_KOT <kod-t@mail.ru>
2019-04-23 18:23:31 +0300

    Add output

--- src/pkgdownloader.py
+++ src/pkgdownloader.py
@@ -30,20 +30,26 @@ def checkLinkStatus(myURL):
     r = http.request('GET', myURL)
     return r.status
 
-def getLink(url):
+def getLink(url, distrib, package):
+    print('[...] Finding links for %s from %s' % (package, distrib))
     linksArray = []
     r = http.request('GET', url)
     soup = BeautifulSoup(r.data, 'html.parser')
     for link in soup.find_all('a', attrs={'href': re.compile('^http://ftp.ru')}):
         if checkLinkStatus(link.get('href')) == 200:
+            print('[!!!] For %s link find' % distrib)
             linksArray.append(link.get('href'))
     return linksArray
 
-def getFileFromURL(url, path, package):
-    for url in getLink(url):
+def getFileFromURL(path, distrib, package):
+    url = 'https://packages.debian.org/' + distrib + '/amd64/' + package + '/download'
+    for url in getLink(url, distrib, package):
+        fileName = str(re.compile(package+'_.*').findall(url)).strip('\"\"\'\'[]')
+        print('[...] Downloading %s' % fileName)
         with http.request('GET', url, preload_content=False) as r, \
-        open(path + '/' + str(re.compile(package+'_.*').findall(url)).strip('\"\"\'\'[]'), 'wb') as out_file:
+        open(path + '/' + fileName, 'wb') as out_file:
             shutil.copyfileobj(r, out_file)
+        print('[+++] Download %s complete' % fileName)
 
 def main():
     if checkLinkStatus('https://packages.debian.org') == 200:
@@ -51,8 +57,6 @@ def main():
             packageDistrib = ['jessie', 'stretch', 'stretch-backports', 'buster', 'sid']
         else:
             packageDistrib = args.packageDistrib
-        print(packageDistrib)
         for package in args.packageName:
             for distrib in packageDistrib:
-                url = 'https://packages.debian.org/' + distrib + '/amd64/' + package + '/download'
-                getFileFromURL(url, args.path, package)
+                getFileFromURL(args.path, distrib, package)
