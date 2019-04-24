31c417442b12c2449f26fd10d962cc4047a5f49c
HEBCKUU_KOT <kod-t@mail.ru>
2019-04-24 13:18:12 +0300

    clean

--- src/pkgdownloader.py
+++ src/pkgdownloader.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import re
 import os
 import shutil
@@ -13,11 +11,15 @@ import argparse
 
 from bs4 import BeautifulSoup
 
-parser = argparse.ArgumentParser(description='Package downloader. Now only from packages.debian.org', usage='%(prog)s -p tmux aptitude vim -d stable -P /dir')
-parser.add_argument('-p', '--package', nargs='*', dest='packageName', help='package name list, use: -p libmsgpack-dev aptitude tmux')
-parser.add_argument('-d', '--distro', nargs='*', dest='packageDistrib', help='dist, use: -d sid. FYI: all for all, jessie, oldstable, stretch, stable, buster, testing, sid, unstable')
-parser.add_argument('-P', '--path', action='store', dest='path', default='/tmp', help='Files saving path, default: /tmp', metavar="FILE")
-args = parser.parse_args()
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Package downloader. Now only from packages.debian.org', usage='%(prog)s -p tmux aptitude vim -d stable -P /dir', add_help=False)
+    parser.add_argument('-p', '--package', nargs='*', dest='packageName', help='package name list, use: -p libmsgpack-dev aptitude tmux')
+    parser.add_argument('-d', '--distro', nargs='*', dest='packageDistrib', help='dist, use: -d sid. FYI: all for all, jessie, oldstable, stretch, stable, buster, testing, sid, unstable')
+    parser.add_argument('-P', '--path', action='store', dest='path', default='/tmp', help='Files saving path, default: /tmp', metavar="FILE")
+    subparsers = parser.add_subparsers()
+    parser_link = subparsers.add_parser('link', parents=[parser], help='Get links, without downloading')
+    parser_deps = subparsers.add_parser('deps', parents=[parser], help='Get dependencies list')
+    return parser.parse_args()
 
 if len(sys.argv) < 2:
     parser.print_help()
@@ -28,25 +30,35 @@ http = urllib3.PoolManager(
         ca_certs=certifi.where())
 
 def checkLinkStatus(myURL):
+
     r = http.request('GET', myURL)
+
     return r.status
 
 def getLink(url, distrib, package):
+
     print('[...] Finding links for %s from %s' % (package, distrib))
+
     linksArray = []
     r = http.request('GET', url)
     soup = BeautifulSoup(r.data, 'html.parser')
+
     for link in soup.find_all('a', attrs={'href': re.compile('^http://ftp.ru')}):
         if checkLinkStatus(link.get('href')) == 200:
             print('[!!!] For %s link find' % distrib)
             linksArray.append(link.get('href'))
+
     return linksArray
 
 def getFileFromURL(path, distrib, package):
+
     url = 'https://packages.debian.org/' + distrib + '/amd64/' + package + '/download'
+
     for url in getLink(url, distrib, package):
+
         fileName = str(re.compile(package+'_.*').findall(url)).strip('\"\"\'\'[]')
         targetFile = path + '/' + fileName
+
         if os.path.isfile(targetFile):
             print('[!!!] File exists')
         else:
@@ -59,12 +71,21 @@ def getFileFromURL(path, distrib, package):
 def getDependenciesFromDSC():
     return
 
+def getRepoPackagesFile():
+#    http://ftp.ru.debian.org/debian/dists/sid/main/binary-amd64/Packages
+    return
+
 def main():
+    args = parseArgs()
     if checkLinkStatus('https://packages.debian.org') == 200:
+
         if args.packageDistrib == ['all']:
             packageDistrib = ['jessie', 'stretch', 'stretch-backports', 'buster', 'sid']
         else:
             packageDistrib = args.packageDistrib
+
         for package in args.packageName:
             for distrib in packageDistrib:
                 getFileFromURL(args.path, distrib, package)
+
+    getRepoPackagesFile()
