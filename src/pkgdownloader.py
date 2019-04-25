86f10c8c9f3c102886a9f46735d5a3d1e62ff6c9
HEBCKUU_KOT <kod-t@mail.ru>
2019-04-25 13:53:47 +0300

    kill multiplies

--- src/pkgdownloader.py
+++ src/pkgdownloader.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Package downloader. Now only from packages.debian.org', usage='%(prog)s -p tmux aptitude vim -d stable -P /dir', add_help=False)
-    parser.add_argument('-a', '--arch', nargs='*', dest='packageArch', help='package arch list, use: -a amd64 i386 arm64, default amd64, not multiply')
+    parser.add_argument('-a', '--arch', action='store', dest='packageArch', help='package arch list, use: -a amd64 i386 arm64, default amd64, not multiply')
     parser.add_argument('-p', '--package', nargs='*', dest='packageName', help='package name list, use: -p libmsgpack-dev aptitude tmux')
     parser.add_argument('-d', '--distro', action='store', dest='packageDistrib', help='dist, use: -d sid. FYI: all for all, jessie, oldstable, stretch, stable, buster, testing, sid, unstable')
     parser.add_argument('-P', '--path', action='store', dest='path', default='/tmp', help='Files saving path, default: /tmp', metavar="FILE")
@@ -42,6 +42,7 @@ def checkLinkStatus(myURL):
 
 def getLink(distrib, package, arch):
     url = 'https://packages.debian.org/' + distrib + '/' + arch + '/' + package + '/download'
+    print(url)
     print('[...] Finding links for %s from %s' % (package, distrib))
     r = http.request('GET', url)
     soup = BeautifulSoup(r.data, 'html.parser')
@@ -59,8 +60,7 @@ def downloadFile(link, targetFile):
             shutil.copyfileobj(r, out_file)
             print('[+++] Complete')
 
-def getRepoPackagesFile(distrib, arch, comp):
-    filename = str(uuid.uuid4())
+def getRepoPackagesFile(distrib, arch, comp, filename):
     targetFile = '/tmp/' + filename + '.gz'
     url = 'http://ftp.ru.debian.org/debian/dists/' + distrib + '/' + comp + '/binary-' + arch + '/Packages.gz'
     downloadFile(url, targetFile)
@@ -76,44 +76,57 @@ def packageDistribList(args):
 def dl(args):
     linksArray = []
     for package in args.packageName:
-        for distrib in packageDistribList(args):
-            for arch in args.packageArch:
-                link = getLink(distrib, package, arch)
-                if link:
-                    linksArray.append(link)
-                else:
-                    print('[ ! ] Link for %s from %s not found' % (package, distrib))
+        link = getLink(args.packageDistrib, package, args.packageArch)
+        if link:
+            linksArray.append(link)
+        else:
+            print('[ ! ] Link for %s from %s not found' % (package, distrib))
 
     for link in linksArray:
-        fileName = str(re.compile('[^/]*$').findall(link)).strip('\"\"\'\'[]')
+        fileName = str(re.compile('[^/]+$').findall(link)).strip('\"\"\'\'[]')
+        print(fileName)
         targetFile = args.path + '/' + fileName
         downloadFile(link, targetFile)
 
 def link(args):
     linksArray = []
     for package in args.packageName:
-        for distrib in packageDistribList(args):
-            for arch in args.packageArch:
-                link = getLink(distrib, package, arch)
-                if link:
-                    linksArray.append(link)
-                else:
-                    print('[ - ] Links not found')
+        link = getLink(args.packageDistrib, package, args.packageArch)
+        if link:
+            linksArray.append(link)
+        else:
+            print('[ - ] Links not found')
 
     for link in linksArray:
         print(link)
 
 def deps(args):
+    filename = str(uuid.uuid4())
     for package in args.packageName:
         for arch in args.packageArch:
             link = getLink(args.packageDistrib, package, arch)
     if link:
         comp = str(re.compile('(?<=pool/)[a-z-]*').findall(link)).strip('\"\"\'\'[]')
         arch = str(re.compile('(?<=_)[a-z].*(?=\.deb)').findall(link)).strip('\"\"\'\'[]')
-        print(comp, arch)
-        getRepoPackagesFile(args.packageDistrib, arch, comp)
+        package = str(re.compile('[^\/][a-z]+?(?=\_)').findall(link)).strip('\"\"\'\'[]')
+        packagesFile = getRepoPackagesFile(args.packageDistrib, arch, comp, filename)
     else:
         print('Link not found')
+        sys.exit
+
+    txtFile = '/tmp/' + filename + '.txt'
+
+    with gzip.open(packagesFile, 'rb') as f_in:
+        with open(txtFile, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+    file_content = open(txtFile, 'r')
+    for line in file_content:
+        if package in line:
+            print(line)
+
+    os.remove(packagesFile)
+    print('%s removed' % packagesFile)
 
 
 def main():
